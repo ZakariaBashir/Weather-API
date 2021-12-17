38ee39d93810d230165766bcc442c4d5530339fe
zakaria bashir <zakaria.bashir98@gmail.com>
2021-12-17 17:05:14 +0200

    Weather API: HTML/CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import Search from "./components/Search";
+// import Search from "./components/Search";
 
-import SayHi, { SayHello } from "./components/WeatherItem";
-import fakeWeatherData from "./fakeWeatherData.json";
-
+// import SayHi, { SayHello } from "./components/WeatherItem";
+// import fakeWeatherData from "./fakeWeatherData.json";
+import mostlycloudy from "./img/weather-icons/mostlycloudy.svg";
+import clear from "./img/weather-icons/clear.svg";
 import "./App.css";
-
+          
 class App extends Component {
   constructor(props) {
     super(props);
@@ -21,9 +22,72 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <SayHi />
-        <SayHello color="black" name={this.state.name} />
-        <Search handleInput={this.handleInputChange} />
+        <body>
+          <header id="header">
+            <input type="text" placeholder="Type in a city name"/>
+            <button className="button">FIND WEATHER</button>
+          </header>
+          
+          <div className="ALL">
+            <div className="part1">
+              <div>
+                <img className="mainImage" src="https://placekitten.com/200/300" alt="mostlycloudy icon" height="202px" />
+                <h3>overcast clouds</h3>
+              </div>
+
+              <div className="temp"><b>Temperature</b>&ensp;&ensp;10&deg; to 11 &#8451;</div>
+              <div>
+                <p className="humid"><b>Humidity</b>&ensp;&ensp;78%&ensp;&ensp;<b>Pressure</b>&ensp;&ensp;1008.48</p>
+              </div>
+      
+            </div>
+            <div className="weather_of_the_day">
+              <div>
+              <p>03:00</p>
+              <img src='https://placekitten.com/200/300' alt="mostlycloudy icon" height="60px"/>
+              <p>8&#8451;</p>
+              </div>
+
+              <div>
+              <p>06:00</p>
+              <img src="https://placekitten.com/200/300" alt="mostlycloudy icon" height="60px"/>
+              <p>9&#8451;</p>
+              </div>
+
+              <div>
+              <p>09:00</p>
+              <img src="https://placekitten.com/200/300" alt="mostlycloudy icon" height="60px"/>
+              <p>14&#8451;</p>
+              </div>
+              
+              <div>
+              <p>12:00</p>
+              <img src="https://placekitten.com/200/300" alt="mostlycloudy icon" height="60px"/>
+              <p>17&#8451;</p>
+              </div>
+              
+              <div>
+              <p>15:00</p>
+              <img src="https://placekitten.com/200/300" alt="mostlycloudy icon" height="60px"/>
+              <p>18&#8451;</p>
+              
+              </div>
+              <div>
+              <p>18:00</p>
+              <img src="https://placekitten.com/200/300" alt="mostlycloudy icon" height="60px"/>
+              <p>16&#8451;</p>
+              </div>
+              
+              <div>
+              <p>21:00</p>
+              <img src="https://placekitten.com/200/300" alt="mostlycloudy icon" height="60px"/>
+              <p>13&#8451;</p>
+              </div>
+
+            </div>
+                
+          </div>
+        </body>
       </div>
     );
   }
